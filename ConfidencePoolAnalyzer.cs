46a01f7a2d5c752ca19565e21627f2cdbda98b6e
juanbond <archaeal@gmail.com>
2014-09-12 00:27:03 -0500

    removed unused directives

--- ConfidencePoolAnalyzer.cs
+++ ConfidencePoolAnalyzer.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
-using System.Security.Cryptography.X509Certificates;
 
 namespace ConfidencePoolAnalyzer
 {
