b8478d33e6728cc0e6c472945d1d3c0e615977ab
juanbond <archaeal@gmail.com>
2014-09-15 16:45:35 -0500

    assembly name

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConfidenceOutcomes")]
+[assembly: AssemblyTitle("ConfidencePoolAnalyzer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConfidenceOutcomes")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyProduct("ConfidencePoolAnalyzer")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
